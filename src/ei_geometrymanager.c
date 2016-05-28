3e8c8412a22f246b0a471794c0c2547254c9d354
Elisa Quenton <Elisa.Quenton@ensimag.grenoble-inp.fr>
2016-05-28 15:51:36 +0200

    ajout d'un geometry_placer.c pour configurer release func et runfunc

--- src/ei_geometrymanager.c
+++ src/ei_geometrymanager.c
@@ -8,6 +8,7 @@
 
 #include "ei_geometrymanager.h"
 #include "debug.h"
+#include "ei_geometry_placer.h"
 /**
  *  @var	gm
  *  @brief	Variable globale contenant un chainage de tous les gm
@@ -16,47 +17,53 @@ ei_geometrymanager_t* gm = NULL;
 
 void ei_geometrymanager_register(ei_geometrymanager_t* geometrymanager)
 {
-    ei_geometrymanager_t* tmp;
-
+  ei_geometrymanager_t* tmp;
+  
     // s'il n'y a pas de classe on la definit comme la premiere
     if(gm == NULL)
-	     gm = geometrymanager;
+	gm = geometrymanager;
     else
     {
-      	tmp = gm;
-      	for(tmp = gm; tmp->next != NULL; tmp = tmp->next);
-      	tmp->next = geometrymanager;
-    }
+	tmp = gm;
+	for(tmp = gm; tmp->next != NULL; tmp = tmp->next);
+	tmp->next = geometrymanager;
 }
+
+
 
 ei_geometrymanager_t* ei_geometrymanager_from_name(ei_geometrymanager_name_t name)
 {
    ei_geometrymanager_t* tmp;
-
+  
     for(tmp = gm; tmp != NULL; tmp = tmp->next)
     {
-      	if(!strcmp(tmp->name, name))
-      	    return tmp;
+	if(!strcmp(tmp->name, name))
+	    return tmp;
     }
-
+    
     PRINT_DEBUG("ei_geometrymanager_from_name : classe non trouvee");
+    
     return NULL;
 }
 
+
+
 void ei_geometrymanager_unmap(ei_widget_t* widget)
 {
-
+  widget->screen_location
 }
+
+
 
 void ei_register_placer_manager()
 {
-    ei_geometrymanager_t* placer = malloc(sizeof(struct(ei_geometrymanager_t));
-    strcpy(placer->name, "placer");
+  ei_geometrymanager_t* placer = malloc(sizeof(struct(ei_geometrymanager_t));
+  strcpy(placer->name, "placer");
   	placer->runfunc=placerRunfunc;
-  	placer->releasefunc=placerReleasefunc;
-  	placer->next=NULL;
-
-    ei_geometrymanager_register(placer);
+	placer->releasefunc=placerReleasefunc;
+	placer->next=NULL;
+	
+  ei_geometrymanager_register(placer);
 }
 
 void	ei_place	(ei_widget_t*		widget,
@@ -70,5 +77,6 @@ void	ei_place	(ei_widget_t*		widget,
 			 float*			rel_width,
 			 float*			rel_height)
 {
-
+  
 }
+
