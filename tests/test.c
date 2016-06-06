98d8e693d061fe10ea1d404b0cae574b1aae274d
Elisa Quenton <Elisa.Quenton@ensimag.grenoble-inp.fr>
2016-06-06 13:39:39 +0200

    jzjei

--- tests/test.c
+++ tests/test.c
@@ -6,33 +6,40 @@
 #include "ei_types.h"
 #include "ei_draw.h"
 #include "hw_interface.h"
-
+#include "ei_widget.h"
+#include "ei_widget_frame.h"
 int ei_main(int argc, char* argv[])
 {
   ei_rect_t rect;
+  ei_linked_rect_t* rect1 = malloc(sizeof(struct ei_linked_rect_t));
+  rect.top_left.x =100;
+  rect.top_left.y =100 ;
+  rect.size.width = 400;
+  rect.size.height = 300;
+  rect1 = &rect;
   ei_point_t centre = {200,200};
   ei_color_t color;
   color.red = 255;
   color.green = 0;
   color.blue = 0;
   color.alpha = 255;
+
   ei_size_t s = {640,480};
-  ei_surface_t surface = hw_create_window(&s,EI_FALSE);
-  ei_linked_point_t* test = malloc(sizeof(struct ei_linked_point_t));
-  rect.top_left.x =100;
-  rect.top_left.y =100 ;
-  rect.size.width = 400;
-  rect.size.height = 300;
+  ei_widget_frame_t* frame = malloc(sizeof(struct ei_widget_frame_t));
   
-  test = round_and_rectangular_frame(rect,15);
+  ei_surface_t surface = hw_create_window(&s,EI_FALSE);
+
+  ei_frame_configure(ei_app_root_widget(),NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+  frame = ei_widget_create("frame",ei_app_root_widget());
 
   hw_init();
   hw_surface_lock(surface);
-  draw_toplevel(rect,1,surface,NULL);
-  //ei_draw_polygon(surface,test,color,NULL);
-  //getchar();
-   
-   hw_surface_unlock(surface);
+
+  ei_frame_configure(frame,&s,&color,NULL,NULL,NULL,NULL,NULL,NULL,hw_image_load("misc/flag.png",surface),&rect1,ei_anc_north);
+  //surface = hw_image_load("misc/ball.png",surface);
+  //drawImgWidget(surface,ei_widget_create(frame,NULL),ei_widget_create(frame,NULL));
+
+  hw_surface_unlock(surface);
   hw_surface_update_rects(surface, NULL);
   getchar(); 
   hw_quit();
